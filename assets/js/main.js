1027a8be3cdd99d6d345430b6c94e0c5612bf474
Théo Jaunet <theo.jaunet.sio@gmail.com>
2026-01-15 09:29:24 +0100

    merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
-
 const myHomography = new homography.Homography();
 
 
-
-
-
-function shapeTrajectory(stripe,points, space,n) {
-
+function shapeTrajectory(stripe, points, space, n) {
 
 
     const w = stripe.width
 
+
+    const step = 1 / n
+
+    const sPts = d3.range(n).map((_, i) =>  step)
+
     // Define the reference points. In this case using normalized coordinates (from 0.0 to 1.0).
     const srcPoints = [[0, 0], [0, 1], [1, 0], [1, 1]];
-    const dstPoints = [[1/5, 1/5], [0, 1/2], [1, 0], [6/8, 6/8]];
+    const dstPoints = [[1 / 5, 1 / 5], [0, 1 / 2], [1, 0], [6 / 8, 6 / 8]];
 
 
     const myHomography = new Homography("piecewiseaffine");
